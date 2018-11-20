d618349fef1013f7aa6d9ef20ea32e6a1a5f76c8
Fukki <takungwong@gmail.com>
2018-11-20 13:55:43 +0700

    check contract and mount

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = function AutoPOT(mod) {
 	let config = getConfig(), hpPot = getHP(), mpPot = getMP();
 	let gameId = null, VehicleEx = null, getInv = false;
 	let isAlive = false, isCombat = false, isBG = false, isSlaying = false;
-	let zoneBG = 0, nowHP = 0, nowMP = 0;
-	
+	let zoneBG = 0, nowHP = 0, nowMP = 0, isCon = false, isMount = false;
+
 	cmd.add(['autopot', 'pot'], (arg1, arg2) => {
 		if(arg1 && arg1.length > 0) arg1 = arg1.toLowerCase();
 		if(arg2 && arg2.length > 0) arg2 = arg2.toLowerCase();
@@ -13,9 +13,9 @@ module.exports = function AutoPOT(mod) {
 			case "load":
 			case "reload":
 				switch(arg2) {
-					case 'hp': hpPot = getHP(); msg(`HP.json has been reloaded.`); break;
-					case 'mp': mpPot = getMP(); msg(`MP.json has been reloaded.`); break;
-					case 'config': config = getConfig(); msg(`Config.json has been reloaded.`); break;
+					case 'hp': hpPot = getHP(); msg(`HP.json has been reloademod.`); break;
+					case 'mp': mpPot = getMP(); msg(`MP.json has been reloademod.`); break;
+					case 'config': config = getConfig(); msg(`Config.json has been reloademod.`); break;
 				}
 				break;
 			case "notice":
@@ -44,6 +44,22 @@ module.exports = function AutoPOT(mod) {
 		}
 	});
 	
+	mod.hook('S_MOUNT_VEHICLE', 2, e => {if (isMe(e.gameId)) isMount = true;});
+	
+	mod.hook('S_UNMOUNT_VEHICLE', 2, e => {if (isMe(e.gameId)) {isMount = false; isCon = false;}});
+	
+	mod.hook('S_REQUEST_CONTRACT', 'raw', () => {isCon = true;});
+	
+	mod.hook('S_ACCEPT_CONTRACT', 'raw', () => {isCon = false;});
+	
+	mod.hook('S_REJECT_CONTRACT', 'raw', () => {isCon = false;});
+	
+	mod.hook('S_CANCEL_CONTRACT', 'raw', () => {isCon = false;});
+	
+	mod.hook('S_GACHA_END', 'raw', () => {isCon = false;});
+	
+	mod.hook('C_BIND_ITEM_EXECUTE', 'raw', () => {isCon = false;});
+	
 	mod.hook("S_LOGIN", 10, e => {({gameId} = e);});
 	
 	mod.hook("S_SPAWN_ME", 3, e => {isAlive = e.alive;});
@@ -58,7 +74,7 @@ module.exports = function AutoPOT(mod) {
 	
 	mod.hook('S_BATTLE_FIELD_ENTRANCE_INFO', 1, e => {zoneBG = e.zone;});
 	
-	mod.hook('S_LOAD_TOPO', 3, e => {isBG = (e.zone === zoneBG); VehicleEx = null;});
+	mod.hook('S_LOAD_TOPO', 3, e => {isBG = (e.zone === zoneBG); VehicleEx = null; isCon = false; isMount = false;});
 	
 	mod.hook('S_INVEN', 16, e => {
 		if (config.enabled) {
@@ -79,7 +95,7 @@ module.exports = function AutoPOT(mod) {
 			if (e.target.equals(gameId)) {
 				nowHP = Math.round(e.curHp / e.maxHp * 100);
 				for (let hp = 0; hp < hpPot.length; hp++) {
-					if (!hpPot[hp][1].inCd && ((!isSlaying && nowHP <= hpPot[hp][1].use_at) || (isSlaying && nowHP <= hpPot[hp][1].slaying)) && hpPot[hp][1].amount > 0 && isCombat && isAlive && !isBG) {
+					if (!hpPot[hp][1].inCd && ((!isSlaying && nowHP <= hpPot[hp][1].use_at) || (isSlaying && nowHP <= hpPot[hp][1].slaying)) && hpPot[hp][1].amount > 0 && isCombat && isAlive && !isBG && !isCon && !isMount) {
 						useItem(hpPot[hp]); hpPot[hp][1].inCd = true; hpPot[hp][1].amount--; setTimeout(function () {hpPot[hp][1].inCd = false;}, hpPot[hp][1].cd * 1000);
 						if (config.notice) msg(`Used ${hpPot[hp][1].name}, still have ${(hpPot[hp][1].amount)} left.`);
 					}
@@ -93,7 +109,7 @@ module.exports = function AutoPOT(mod) {
 			if (e.target.equals(gameId)) {
 				nowMP = Math.round(e.currentMp / e.maxMp * 100);
 				for (let mp = 0; mp < mpPot.length; mp++) {
-					if (!mpPot[mp][1].inCd && nowMP <= mpPot[mp][1].use_at && mpPot[mp][1].amount > 0 && isAlive && !isBG) {
+					if (!mpPot[mp][1].inCd && nowMP <= mpPot[mp][1].use_at && mpPot[mp][1].amount > 0 && isAlive && !isBG && !isCon && !isMount) {
 						useItem(mpPot[mp]); mpPot[mp][1].inCd = true; mpPot[mp][1].amount--; setTimeout(function () {mpPot[mp][1].inCd = false;}, mpPot[mp][1].cd * 1000);
 						if (config.notice) msg(`Used ${mpPot[mp][1].name}, still have ${(mpPot[mp][1].amount)} left.`);
 					}
